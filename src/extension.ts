85feb9813615d6eb2f826e498b04b65840e2a350
guillaume <guillaume.pouget@thalesgroup.com>
2022-07-29 15:56:37 +0200

    add comment

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { PunchDocumentRangeFormatter, MODE } from './formatter';
 import { runPuncher } from './puncher';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	// class for formatting function
 	const formatter = new PunchDocumentRangeFormatter();
 
+
+	// Command Create New Punchlet 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('punch.Punchlet', () => {
 			vscode.workspace.openTextDocument({
@@ -18,12 +22,14 @@ export function activate(context: vscode.ExtensionContext) {
 			})
 		})
 	);
-
+	
+	// Command Run Puncher
 	context.subscriptions.push(vscode.commands.registerCommand('punch.Puncher', () => {
 			runPuncher(vscode.window.activeTextEditor?.document.fileName)
 		})
 	);
-
+	
+	// Formatting Provider
 	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(MODE, formatter));
 }
 
