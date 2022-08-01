9d134a747c5787a39e695d63b073047096b31bb3
guillaume <guillaume.pouget@thalesgroup.com>
2022-08-01 17:54:16 +0200

    fix formatting error

--- formatter/formatter.py
+++ formatter/formatter.py
@@ -10,6 +10,7 @@ class Formatter:
     # indentation
     ilvl = 0
     iwidth = 0
+    isString = (False, '\"')
 
     def __init__(self, indentwidth):
         self.iwidth = indentwidth
@@ -32,43 +33,68 @@ class Formatter:
         if re.match(self.line_comment, line):
             return (0, self.indent() + line.strip())
 
-        # find if there " or ' in line
-        string_in_line = [m for m in re.findall(r'\"|\'',line)]
+        # get all ", ', {, }, for and ; in the current line 
+        find_all_element = [m for m in re.finditer(r'\"|\'|{|}|;|\s*for\s*\(',line)]
+        list_for = []
 
-        # if " or ' just indent line 
-        if len(string_in_line) > 0 :
+        # search if there is any for statement
+        for i in range(0, len(find_all_element)-1) :
+            if(find_all_element[i][0] == 'for (') :
+                list_for.append(i)
+
+        # remove the 2 next ; if there is for
+        for e in list_for:
+            del find_all_element[e:e+3]
+
+        # return indent line if no special charater
+        if (len(find_all_element) < 1): 
             return (0, self.indent() + line.strip())
-        
-        semicolon = [m for m in re.finditer(";",line)]
-        open_parenthesis = [m for m in re.finditer("{",line)]
-        close_parenthesis = [m for m in re.finditer("}",line)]
 
-        # format if one or multiple { in line 
-        if len(open_parenthesis) > 0 :
-            result = self.indent() + line[0:open_parenthesis[0].span()[1]]
-            self.ilvl +=4
-            for i in range(0,len(open_parenthesis)-1):
-                result += self.indent() + line[open_parenthesis[i].span()[1]:open_parenthesis[i+1].span()[1]].strip()
-                self.ilvl +=4
-            return(0, result)
-            
-        # format if one or multiple } in line
-        if len(close_parenthesis)>0 : 
-            self.ilvl -= 4
-            result = self.indent() + line[0:close_parenthesis[0].span()[1]]
-            for i in range(0,len(close_parenthesis)-1):
+        # return indent line if 1 special charater and maj ident
+        if (len(find_all_element) == 1): 
+            if find_all_element[0][0] == '{' :
+                return (4, self.indent() + line.strip())
+            if find_all_element[0][0] == '}' :
                 self.ilvl -= 4
-                result += '\n' + self.indent() + line[close_parenthesis[i].span()[1]:close_parenthesis[i+1].span()[1]].strip()
-            return(0, result) 
+            return (0, self.indent() + line.strip())
 
-        # format if multiple ; in line 
-        if len(semicolon) > 1 : 
-            f = [m for m in re.findall(r'\s*for\s*\(',line)]
-            if len(f) < 1 : 
-                return self.multiple_semicolon(line, semicolon)
+        # format if there more than on special charater
+        result = self.indent() + line[0:find_all_element[0].span()[1]]
+        for i in range(0, len(find_all_element)-1)  :
+            if self.isString == (True, find_all_element[i][0]):
+                self.isString = (False, self.isString[1])
+                result += line[find_all_element[i].span()[1]:find_all_element[i+1].span()[1]]
+                continue
+
+            if find_all_element[i][0] == '\"' or find_all_element[i][0] == '\'':
+                self.isString = (True, find_all_element[i][0])
+                result += line[find_all_element[i].span()[1]:find_all_element[i+1].span()[1]]
+                continue
+
+            if self.isString[0] == True : 
+                result += line[find_all_element[i].span()[1]:find_all_element[i+1].span()[1]]
+                continue
+
+            if find_all_element[i][0] =='{' :
+                self.ilvl += 4
+                result += '\n' + self.indent() +line[find_all_element[i].span()[1]:find_all_element[i+1].span()[1]].strip()
+
+            if find_all_element[i][0] ==';' :
+                if find_all_element[i+1][0] == '}' :
+                    self.ilvl -= 4
+                result += '\n' + self.indent() +line[find_all_element[i].span()[1]:find_all_element[i+1].span()[1]].strip()
+
+            if find_all_element[i][0] =='}' :
+                self.ilvl -= 4
+                result += '\n' + self.indent() +line[find_all_element[i].span()[1]:find_all_element[i+1].span()[1]].strip()
         
-        # if no special caraters just indent
-        return (0, self.indent() + line.strip())
+        result = result + line[find_all_element[len(find_all_element)-1].span()[1] : len(line)]
+        if find_all_element[len(find_all_element)-1][0] == '{' :
+                self.ilvl += 4
+        if find_all_element[len(find_all_element)-1][0] == '}' :
+                self.ilvl -= 4
+
+        return (0, result)
         
 
 
@@ -95,6 +121,8 @@ class Formatter:
         for line in rlines:
             # format line
             (offset, line) = self.formatLine(line)
+            self.ilvl += offset
+
             # add line to result
             wlines.append(line)
 
